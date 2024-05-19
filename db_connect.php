3a0e66206938a444c5ff5bb820fc42301831bb7e
hanjegoddamn <iplaynamoment@gmail.com>
2024-05-19 11:09:37 +0600

    Initial commit

---
+++ db_connect.php
@@ -0,0 +1,2 @@
+<?php
+$db = mysqli_connect("7ny.h.filess.io", "authorizationtutorial_richdrawn", "5a08ac68a198322f727e6bc402fc99a1b292afa2", "authorizationtutorial_richdrawn");
---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include("db_connect.php");
+$email = $_POST['email'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$repeatpassword = $_POST['repeatpassword'];
+$md5_password = md5($password);
+$query = mysqli_query($db, "SELECT * FROM `users` WHERE `login`='{$login}'");
+if (mysqli_num_rows($query) == 0) {
+    $_SESSION['user'] = ['nick' => $login];
+    mysqli_query($db, "INSERT INTO `users` (`login`, `password`) VALUES ('{$login}', '{$md5_password}')");
+    header("Location: /user.php");
+} else {
+    echo("Ошибка: Данный логин занят другим пользователем.");
+}
